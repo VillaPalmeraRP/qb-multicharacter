4dd5e410dbb282c2947d7d098abf7734c9b609fb
LoyaltyDeadShot <deadshotpr642@gmail.com>
2022-08-31 20:38:34 -0400

    css modification and change coords

--- config.lua
+++ config.lua
@@ -1,12 +1,12 @@
 Config = {}
 Config.Interior = vector3(-814.89, 181.95, 76.85) -- Interior to load where characters are previewed
 Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords if you have start apartments disabled
-Config.PedCoords = vector4(-813.97, 176.22, 76.74, -7.5) -- Create preview ped at these coordinates
-Config.HiddenCoords = vector4(-812.23, 182.54, 76.74, 156.5) -- Hides your actual ped while you are in selection
-Config.CamCoords = vector4(-813.46, 178.95, 76.85, 174.5) -- Camera coordinates for character preview screen
+Config.PedCoords = vector4(-1084.35, -2728.49, 0.81, 289.12) -- Create preview ped at these coordinates
+Config.HiddenCoords = vector4(-1044.81, -2749.69, 21.36, 336.72) -- Hides your actual ped while you are in selection
+Config.CamCoords = vector4(-1081.47, -2727.25, 0.81, 111.35) -- Camera coordinates for character preview screenn
 Config.EnableDeleteButton = true -- Define if the player can delete the character or not
 
-Config.DefaultNumberOfCharacters = 5 -- Define maximum amount of default characters (maximum 5 characters defined by default)
+Config.DefaultNumberOfCharacters = 1 -- Define maximum amount of default characters (maximum 5 characters defined by default)
 Config.PlayersNumberOfCharacters = { -- Define maximum amount of player characters by rockstar license (you can find this license in your server's database in the player table)
     { license = "license:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", numberOfChars = 2 },
 }
